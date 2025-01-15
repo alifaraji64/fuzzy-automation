f789a462e84ed690856caf61a217e717b87f03f8
alifaraji <alifaraji65452@gmail.com>
2025-01-16 00:09:14 +0330

    first page done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,40 +1,70 @@
-import { ContainerScroll } from "@/components/container-scroll";
+import { ContainerScroll } from "@/components/global/container-scroll";
+import { InfiniteMovingCards } from "@/components/global/infinite-moving-cards";
 import Navbar from "@/components/global/navbar";
 import { Button } from "@/components/ui/button";
-import { h1 } from "framer-motion/client";
+import { HeroParallax } from "@/components/ui/hero-parallax";
+import { LampDemo } from "@/components/ui/lamp";
+import { clients, products } from "@/lib/constants";
+import Link from "next/link";
+import { CheckIcon } from "lucide-react";
 import Image from "next/image";
+import ThreeDCard from "@/components/global/ThreeDCardContainer";
 
 export default function Home() {
   return (
     <main>
       <Navbar></Navbar>
       <section className="h-screen w-full
-     bg-neutral-950 rounded-md !overflow-scroll 
+     bg-neutral-950 rounded-md !overflow-scroll
      relative flex flex-col items-center antialiased">
-        <div className="max-w-4xl mx-auto p-4 
-      relative z-10 flex items-center 
+        <div className="max-w-4xl mx-auto p-4
+      relative z-10 flex items-center
       justify-center flex-col"></div>
-        <div className="absolute inset-0 
+        <div className="absolute inset-0
        h-full w-full items-center px-5 py-24
         [background:radial-gradient(125%_125%_at_50%_10%,#000_35%,#223_100%)]"></div>
         <div className="flex flex-col mt-[-100px] md:mt-[-50px]">
-        <ContainerScroll
-            titleComponent={
-              <div className="flex items-center flex-col">
-                <Button
-                  size={'lg'}
-                  className="p-8 mb-8 md:mb-0 text-2xl w-full sm:w-fit border-t-2 rounded-full border-[#4D4D4D] bg-[#1F1F1F] hover:bg-white group transition-all flex items-center justify-center gap-4 hover:shadow-xl hover:shadow-neutral-500 duration-500"
-                >
-                  <span className="bg-clip-text text-transparent bg-gradient-to-r from-neutral-500 to-neutral-600  md:text-center font-sans group-hover:bg-gradient-to-r group-hover:from-black goup-hover:to-black">
-                    Start For Free Today
-                  </span>
-                </Button>
-                <h1 className="text-5xl md:text-8xl  bg-clip-text text-transparent bg-gradient-to-b from-white to-neutral-600 font-sans font-bold">
-                  Automate Your Work With Fuzzie
-                </h1>
-              </div>
-            }
-          />
+          <ContainerScroll titleComponent={<div className="flex items-center flex-col">
+            <Button
+              size={'lg'}
+              className="p-8 mb-8 md:mb-0 text-2xl w-full sm:w-fit border-t-2 rounded-full border-[#4D4D4D] bg-[#1F1F1F] hover:bg-white group transition-all flex items-center justify-center gap-4 hover:shadow-xl hover:shadow-neutral-500 duration-500"
+            >
+              <span className="bg-clip-text text-transparent bg-gradient-to-r from-neutral-500 to-neutral-600  md:text-center font-sans group-hover:bg-gradient-to-r group-hover:from-black goup-hover:to-black">
+                Start For Free Today
+              </span>
+            </Button>
+            <h1 className="mb-10 text-5xl md:text-8xl  bg-clip-text text-transparent bg-gradient-to-b from-white to-neutral-600 font-sans font-bold">
+              Automate Your Work With Fuzzie
+            </h1>
+          </div>}
+          >
+
+            <Image
+              src={`/assets/temp-banner.png`}
+              alt="hero"
+              height={720}
+              width={1400}
+              className="mx-auto rounded-2xl object-cover h-full object-left-top"
+            />
+
+          </ContainerScroll>
+        </div>
+      </section>
+      <InfiniteMovingCards
+        items={clients}
+        direction="right"
+        speed="slow"
+      />
+      <section>
+        <HeroParallax products={products} />
+      </section>
+      <section className="">
+        <LampDemo></LampDemo>
+        <div className="flex items-center justify-center flex-wrap
+        flex-col md:flex-row gap-8">
+          <ThreeDCard title="Hobby" price={0}/>
+          <ThreeDCard title="Pro Plan" price={29}/>
+          <ThreeDCard title="Unlimited" price={99}/>
         </div>
       </section>
     </main>
---
+++ src/lib/constants.ts
@@ -0,0 +1,83 @@
+export const clients = [...new Array(10)].map((client, index) => ({
+    href: `/assets/${index + 1}.png`,
+  }))
+  export const products = [
+    {
+      title: 'Moonbeam',
+      link: 'https://gomoonbeam.com',
+      thumbnail: '/assets/p1.png',
+    },
+    {
+      title: 'Cursor',
+      link: 'https://cursor.so',
+      thumbnail: '/assets/p2.png',
+    },
+    {
+      title: 'Rogue',
+      link: 'https://userogue.com',
+      thumbnail: '/assets/p3.png',
+    },
+
+    {
+      title: 'Editorially',
+      link: 'https://editorially.org',
+      thumbnail: '/assets/p4.png',
+    },
+    {
+      title: 'Editrix AI',
+      link: 'https://editrix.ai',
+      thumbnail: '/assets/p5.png',
+    },
+    {
+      title: 'Pixel Perfect',
+      link: 'https://app.pixelperfect.quest',
+      thumbnail: '/assets/p6.png',
+    },
+
+    {
+      title: 'Algochurn',
+      link: 'https://algochurn.com',
+      thumbnail: '/assets/p1.png',
+    },
+    {
+      title: 'Aceternity UI',
+      link: 'https://ui.aceternity.com',
+      thumbnail: '/assets/p2.png',
+    },
+    {
+      title: 'Tailwind Master Kit',
+      link: 'https://tailwindmasterkit.com',
+      thumbnail: '/assets/p3.png',
+    },
+    {
+      title: 'SmartBridge',
+      link: 'https://smartbridgetech.com',
+      thumbnail: '/assets/p4.png',
+    },
+    {
+      title: 'Renderwork Studio',
+      link: 'https://renderwork.studio',
+      thumbnail: '/assets/p5.png',
+    },
+
+    {
+      title: 'Creme Digital',
+      link: 'https://cremedigital.com',
+      thumbnail: '/assets/p6.png',
+    },
+    {
+      title: 'Golden Bells Academy',
+      link: 'https://goldenbellsacademy.com',
+      thumbnail: '/assets/p1.png',
+    },
+    {
+      title: 'Invoker Labs',
+      link: 'https://invoker.lol',
+      thumbnail: '/assets/p2.png',
+    },
+    {
+      title: 'E Free Invoice',
+      link: 'https://efreeinvoice.com',
+      thumbnail: '/assets/p3.png',
+    },
+  ]
